92d68550fd7cb9ae38ebae99c66e746be37ba68d
pinkkykuki <panpink1509@gmail.com>
2025-12-26 02:03:07 +0700

    Update lab10_3.cpp

--- lab10_3.cpp
+++ lab10_3.cpp
@@ -1,3 +1,27 @@
-//cout << "Number of data = ";
-//cout << "Mean = ";
-//cout << "Standard deviation = ";
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main(){
+    int count = 0;
+    float sum = 0;
+    float sum_s = 0;
+    string line;
+    ifstream source("score.txt");
+    while (getline(source,line)) {
+        float value = atof(line.c_str());
+        sum += value;
+        sum_s += value*value;
+        count++;
+    }
+    cout << "Number of data = "<<count<<endl;
+    cout << setprecision(3);
+    float mean = sum/count;
+    cout << "Mean = "<<mean<<endl;
+    float variance = (sum_s/count) - (mean * mean);
+    float std_de = sqrt(variance);
+    cout << "Standard deviation = "<<std_de<<endl;
+}
