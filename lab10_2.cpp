ba2d13836e9ce9d71e83babeedbcc314abce9c91
pinkkykuki <panpink1509@gmail.com>
2025-12-26 02:01:51 +0700

    Update lab10_2.cpp

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -6,12 +6,17 @@ using namespace std;
 int main (){
 	ifstream source;
 	ofstream dest;
-	source.open("cheerbook.txt") ;
+	source.open("cheerbook.txt");
 	dest.open("cheerbook_copy.txt");
 	
 	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
 	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
-
+    dest<<"-------------------- BOOM ---------------------\n";
+    string line;
+    while (getline(source,line)) {
+        cout<<line<<endl;
+    }
+    dest<<"-------------------- HA!! ---------------------";
     source.close();
     dest.close();
 	return 0;
